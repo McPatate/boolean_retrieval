d489a58926ae0f3e9e6a244f53e1df94960b381e
McPotato <luc.sydney.georges@gmail.com>
2021-01-23 18:57:41 +0100

    added save function to inverted index

--- src/inverted_index.rs
+++ src/inverted_index.rs
@@ -1,5 +1,9 @@
+use crate::wiki::WikiDoc;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufWriter;
 
 #[derive(Serialize, Deserialize)]
 pub struct InvertedIndex {
@@ -7,8 +11,14 @@ pub struct InvertedIndex {
 }
 
 impl InvertedIndex {
-    pub fn save() {}
-    pub fn build() {}
+    pub fn save(&self, fp: &str) -> std::io::Result<()> {
+        let file = File::open(fp)?;
+        let mut bw = BufWriter::new(file);
+        serde_json::to_writer(&mut bw, &self.idx)?;
+        Ok(())
+    }
+
+    pub fn add_wiki_doc(doc: WikiDoc) {}
     pub fn search(query: &str) {}
     fn parse_query() {}
     fn intersect_not() {}
