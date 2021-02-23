0edf3b88fbb09af6eadcc6a42c5b48edee26c0f5
McPotato <luc.sydney.georges@gmail.com>
2021-02-23 10:05:45 +0100

    ord trait for block

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 /// `Block` is the smallest unit of data
 /// it contains a meta data about the block, the key & the value
 ///
@@ -8,10 +10,29 @@
 /// | uuid (u128) | deleted (bool) | version(u32) | key (Vec<u8>) | value (u32) |
 /// +-------------+----------------+--------------+---------------+-------------+
 /// ```
-
 struct Block {
     pub id: u128,
     pub deleted: bool,
     pub key: Vec<u8>,
     pub value: Option<Vec<u8>>,
+}
+
+impl Eq for Block {}
+
+impl PartialEq for Block {
+    fn eq(&self, other: &Self) -> bool {
+        self.key == other.key
+    }
+}
+
+impl Ord for Block {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.key.cmp(&other.key)
+    }
+}
+
+impl PartialOrd for Block {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
